da5fcee75fc1f4816040077bb9bf5edda855f6e6
Skyler Leigh <nevermaker@gmail.com>
2012-02-23 23:48:58 -0800

    Only print if verbose option set

--- github_status_checker.py
+++ github_status_checker.py
@@ -5,7 +5,7 @@ import simplejson
 # super secret status API
 url = 'https://status.github.com/status.json'
 
-# status codes by color
+# status codes
 # ordered by anti-rainbow, also octocat happiness level
 status_code = {
     'good': 0,
@@ -13,13 +13,21 @@ status_code = {
     'majorproblem': 1
 }
 
-def check_status():
+def check_status(verbose=False):
     r = requests.get(url)
     r.raise_for_status()
     s = simplejson.loads(r.text)
     status, last_updated = s['status'], s['last_updated']
-    print "Github status %s on %s" % (status, last_updated)
+    if verbose:
+        print "Github status %s on %s" % (status, last_updated)
     return status_code[status]
+    
+def main():
+    parser = argparse.ArgumentParser(description="GitHub status checker.")
+    parser.add_argument('-v', '--verbose', dest='verbose', action='store_true',
+                        help="print status to stdout")
+    args = parser.parse_args()
+    check_status(args.verbose)
 
 if __name__ == '__main__':
-    check_status()
+    main()
